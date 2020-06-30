0ae430c1fb7b6169a7331d0e1d551f2a4e355017
Sultan Kenjeyev <skenjeye@broadinstitute.org>
2020-06-30 14:13:26 -0400

    model complexity experiment; updated thresholdout to enable logging

--- thresholdout.py
+++ thresholdout.py
@@ -1,9 +1,8 @@
 from torch.distributions.laplace import Laplace
-
-
+import pandas as pd
 
 class Thresholdout:
-    def __init__(self, train, holdout, tolerance=0.01/4, scale_factor=4):
+    def __init__(self, train, holdout, tolerance=0.01/4, scale_factor=4, keep_log=True):
         self.tolerance = tolerance
         
         self.laplace_eps = Laplace(torch.tensor([0.0]), torch.tensor([2*self.tolerance]))
@@ -14,11 +13,17 @@ class Thresholdout:
         self.holdout = holdout
         
         self.T = 4*tolerance + self.noise(self.laplace_gamma)
+        # self.budget = ???
+        
+        self.keep_log = keep_log
+        if keep_log:
+            self.log = pd.DataFrame(columns=['GlobStep', 'threshold', 'delta', 'phi_train', 'phi_holdout', 'estimate', 'overfit'])
+        
         
     def noise(self, dist):
         return dist.sample().item()
         
-    def verify_statistic(self, phi):
+    def verify_statistic(self, phi, glob_step=None):
         """
             - phi(dataset) -> statistic: 
               function returns the average of some statistic
@@ -28,9 +33,18 @@ class Thresholdout:
         holdout_val = phi(self.holdout)
                 
         delta = abs(train_val - holdout_val)
+        thresh = self.T + self.noise(self.laplace_eta)
         
-        if delta > self.T + self.noise(self.laplace_eta):
+        if delta > thresh:
             self.T += self.noise(self.laplace_gamma)
-            return holdout_val + self.noise(self.laplace_eps), delta, False
+            estimate = holdout_val + self.noise(self.laplace_eps)
         else:
-            return train_val, delta, True
+            estimate = train_val
+            
+        if self.keep_log:
+            if glob_step is None: 
+                raise ValueException('please provide glob step if logging is on')
+            self.log.loc[len(self.log)] = [glob_step, thresh, delta, train_val, holdout_val, estimate, delta > thresh]
+            
+        return estimate
+            
